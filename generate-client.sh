80224e60aea9e86469c188341f78acc42a7b635e
ptylczynski <piotr@ptl.cloud>
2022-08-03 12:23:30 +0200

    change: add auto-version add bash script part

--- generate-client.sh
+++ generate-client.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -exo pipefail
+# set -exo pipefail
 
 echo "api specification url: $1"
 
@@ -15,3 +15,17 @@ docker run \
           --git-user-id smartization \
           -c /local/config.json \
           -DbrowserClient=false
+
+echo "Adding language version 2.9 to all files"
+
+for FILE in $(find . -name '*.dart'); do
+  # check if file begins with version annotation
+  if [[ $(sed -n '/^\/\/ @dart=2.9/p;q' $FILE) ]]; then
+    # begins
+    echo "$FILE already contains proper dart version annotation"
+  else
+    # does not begins
+    echo "$FILE does not contains proper dart version annotation, adding it"
+    echo -e "// @dart=2.9\n$(cat $FILE)" > $FILE
+  fi
+done
