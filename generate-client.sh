5f91fdc29584cd43124d226d99f5e5c58e0641fd
ptylczynski <piotr@ptl.cloud>
2022-08-03 12:52:42 +0200

    change: add version autoincrement

--- generate-client.sh
+++ generate-client.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 # set -exo pipefail
 
+# increment version by one subversion
+major_version=$(cat config.json | grep 1 | cut -d : -f 2 | tr -d \" | tr -d \, | tr -d \ | cut -d \. -f 1)
+minor_version=$(cat config.json | grep 1 | cut -d : -f 2 | tr -d \" | tr -d \, | cut -d \. -f 2)
+sub_version=$(cat config.json | grep 1 | cut -d : -f 2 | tr -d \" | tr -d \, | cut -d \. -f 3)
+echo "Current version: "
+echo "Major version: $major_version"
+echo "Minor version: $minor_version"
+echo "Subversion: $sub_version"
+echo -e "\n"
+echo "New version: "
+echo "Major version: $major_version"
+echo "Minor version: $minor_version"
+echo "Subversion: $(( sub_version + 1 ))"
+
+sed -i "s/$major_version.$minor_version.$sub_version/$major_version.$minor_version.$(( sub_version + 1 ))/g" config.json
+
 echo "api specification url: $1"
 
 docker run \
@@ -29,3 +45,6 @@ for FILE in $(find . -name '*.dart'); do
     echo -e "// @dart=2.9\n$(cat $FILE)" > $FILE
   fi
 done
+
+echo "Adding sdk version to pubspec.yaml"
+echo -e "environment:\n\tsdk: \">=2.17.5 <3.0.0\"" >> pubspec.yaml
